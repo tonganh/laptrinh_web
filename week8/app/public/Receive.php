b81e7393b9681ccbcd9e8d4d78999bf355e71876
anhtn <tongngocanhcampha@gmail.com>
2021-11-19 15:47:28 +0700

    week8

---
+++ week8/app/public/Receive.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <title>Insert Results</title>
+</head>
+
+<body>
+    <?php
+    $host = 'db';
+    $user = 'root';
+    $passwd = '';
+    $database = 'exercise';
+    $connect = mysqli_connect($host, $user, $passwd, $database);
+
+    $username = $_POST["username"];
+    $username = mysqli_real_escape_string($connect, $username);
+    $password = $_POST["password"];
+    $password = mysqli_real_escape_string($connect, $password);
+
+
+    $table_name = 'user';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $query = "select * from $table_name where username = '$username' ";
+        print "The Query is <i>$query</i><br>";
+        if ($data = $connect->query($query)) {
+            print "Query was successful!";
+        } else {
+            print "Query failed!";
+        }
+        //        echo "<br/>data: $data";
+        foreach ($data as $row) {
+            foreach ($row as $column) {
+                echo "<br/>$column";
+            }
+        }
+    }
+
+    mysqli_close($connect);
+    ?>
+</body>
+
+</html>
---
+++ week8/app/public/user-db.php
@@ -0,0 +1,23 @@
+<?php
+$host = 'db';
+$user = 'root';
+$passwd = '';
+$database = 'exercise';
+$connect = mysqli_connect($host, $user, $passwd, $database);
+$table_name = 'user';
+
+
+$SQLcmd = "CREATE TABLE $table_name(
+        username VARCHAR(30) PRIMARY KEY,
+        password VARCHAR(30)
+    )";
+
+print "The Query is <i>$SQLcmd</i><br>";
+if ($connect->query($SQLcmd)) {
+    print "Create $database was successful!</font>";
+} else {
+    print "Create $database failed!</font>";
+}
+
+
+mysqli_close($connect);
